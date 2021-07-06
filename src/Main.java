c97aab1fbf37babfa7a3bbb90c6b14c1392e74de
nickwhitley <nicrofilm@gmail.com>
2021-07-05 21:23:55 -0400

    Testing commit

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+import Database.AccountDB;
+import Database.CustomerDB;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /**
+         * ui manager
+         * account
+         * customer
+         * transactions
+         *
+         */
+
+        //Troubleshooting section Begin
+
+
+        AccountDB accountDB = new AccountDB();
+        accountDB.printAllAccountsAllData();
+//        accountDB.deleteTable();
+
+        CustomerDB customerDB = new CustomerDB();
+//        customerDB.createCustomer("sample","sample","sample","sample","sample");
+        customerDB.printAllCustomersAllData();
+//        customerDB.deleteAllDataEntries();
+        //test git test
+
+
+
+        //Troubleshooting section ends
+
+
+
+
+        UIManger uiManger = new UIManger();
+        uiManger.displayInitialMenu();
+    }
+
+}
